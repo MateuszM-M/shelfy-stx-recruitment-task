e2511edce5ee8c076cd171e5565930dc5c7fa286
MateuszM-M <mateuszmikulski2@gmail.com>
2022-01-15 18:16:11 +0100

    Create simple book database
    Create simplified database and add admin panel as a base
    for future improvements of db, and creating views.

--- books/admin.py
+++ books/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import Book, Author
 
-# Register your models here.
+
+class AuthorInline(admin.StackedInline):
+    model = Author
+    extra = 1
+    
+    
+class BookAdmin(admin.ModelAdmin):
+    inlines = [AuthorInline]
+    list_display = ['title','get_authors', 'published_date', 'isbn', 
+                    'page_count', 'language']
+
+    def get_authors(self, obj):
+        authors = []
+        for author in obj.authors.all():
+            authors.append(author)
+        return authors
+    get_authors.short_description = 'Author'
+
+admin.site.register(Book, BookAdmin)
--- books/models.py
+++ books/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    published_date = models.DateField()
+    isbn = models.DecimalField(max_digits=13, decimal_places=0)
+    page_count = models.DecimalField(max_digits=5, decimal_places=0)
+    image_link = models.URLField(max_length=500)
+    language = models.CharField(max_length=2)
+    
+    def __str__(self):
+        return self.title
+    
+    
+class Author(models.Model):
+    author = models.CharField(max_length=200)
+    book = models.ForeignKey(Book, 
+                            related_name='authors', 
+                            on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.author
